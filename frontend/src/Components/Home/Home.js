c6be73d2455498de37f42492cedce5988321e9b8
PanchaliUpendra <upendrapanchali83@gmail.com>
2024-04-22 14:51:09 +0530

    home page completed

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -5,8 +5,33 @@ import weltvis from './hassets/discussion.jpg';
 
 import Footer from '../Footer/Footer';
 
+//lets import the images
+import vis1 from './hassets/vis1.jpg';
+import vis2 from './hassets/vis2.jpg';
+import vis3 from './hassets/vis3.jpg';
+import vis4 from './hassets/vis4.jpg';
+import vis5 from './hassets/vis5.jpg';
+
+//three curriculum images
+import three1 from './hassets/three1.png';
+import three2 from './hassets/three2.png';
+import three3 from './hassets/three3.png';
+
+//why vis
+ import hwd1 from './hassets/hwd1.png';
+ import hwd2 from './hassets/hwd2.png';
+ import hwd3 from './hassets/hwd3.png';
+ import hwd4 from './hassets/hwd4.png';
+ import hwd5 from './hassets/hwd5.png';
+
+ //chairman message
+ import cm1 from '../../Assets/cm1.jpeg';
+ import cm2 from '../../Assets/cm2.jpeg';
+import { useNavigate } from 'react-router-dom';
+
 function Home(){
     const [philosophy,setphilosophy] = useState(false);
+    const navigate = useNavigate();
     useEffect(()=>{
         window.scrollTo({top:0,behavior:'instant'});
     },[]);
@@ -19,6 +44,121 @@ function Home(){
                     </div>
                 </div>
             </div>
+            
+            <div className='home-page-vis-meru'>
+                <h1>Welcome to vivekananda international school</h1>
+                <h2>As one of the nurturing schools in Hyderabad, we are a caring community school that focuses on the development and well-being of the child.</h2>
+                <p>At International Schools, VIS strives to ensure that every child is provided with everything necessary for their success. Focused on molding global 
+                    citizens, this international school enables young minds to not only reach the peak of their endeavors but to do so in a responsible and conscientious manner. 
+                    VIS CBSE Schools believes that it’s not just about the destination, but the journey that contributes to the holistic growth of children.
+                </p>
+
+                <div className='home-remain-vis15'>
+                    <div>
+                        <img src={vis1} alt='vis1'/>
+                        <h1>5 Acre Campus</h1>
+                    </div>
+                    <div>
+                        <img src={vis2} alt='vis2'/>
+                        <h1>800+ Students & 110+ Staff</h1>
+                    </div>
+                    <div>
+                        <img src={vis3} alt='vis3'/>
+                        <h1>Cambridge & CBSE Curriculum</h1>
+                    </div>
+                    <div>
+                        <img src={vis4} alt='vis4'/>
+                        <h1>10k+ Books in the Library</h1>
+                    </div>
+                    <div>
+                        <img src={vis5} alt='vis5'/>
+                        <h1>250 Security Cameras & 50 Security personnel</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div className='home-page-curriculum'>
+                <h1>Our Curriculum</h1>
+                <p>VIS, renowned for its excellence in education, ensures that every child is equipped with all they need to succeed. Our CBSE schools in Hyderabad are 
+                    dedicated to molding citizens who not only achieve their aspirations but also demonstrate responsibility and conscientiousness. At VIS International Schools, 
+                    we believe in nurturing not just academic success, but also fostering a journey of personal growth for every child.
+                </p>
+            </div>
+            <div className='homepage-three-curriculum'>
+                <h1>We have divided our curriculum into 3 parts</h1>
+
+                <div className='homepage-three-curriculum-boxes'>
+                    <div className='homepage-three-curriculum-box-each'>
+                        <div>
+                            <img src={three1} alt='three1'/>
+                        </div>
+
+                        <h1>The</h1>
+                        <h1>foundational</h1>
+                        <h1>stage</h1>
+                        <p>class 1 to 2</p>
+                        <p>( 6 to 8 years )</p>
+                        
+                    </div>
+                    <div className='homepage-three-curriculum-box-each'>
+                        <div>
+                            <img src={three2} alt='three2'/>
+                        </div>
+                        <h1>The</h1>
+                        <h1>preporatory</h1>
+                        <h1>stage</h1>
+                        <p>class 3 to 5</p>
+                        <p>( 8 to 11 years )</p>
+                    </div>
+                    <div className='homepage-three-curriculum-box-each'>
+                        <div>
+                            <img src={three3} alt='three3'/>
+                        </div>
+                        <h1>the</h1>
+                        <h1>middle</h1>
+                        <h1>stage</h1>
+                        <p>class 6 to 9</p>
+                        <p>( 11 to 14 years )</p>
+                    </div>
+                </div>
+            </div>
+            <div className='homepage-wwhy-vis'>
+                <h1>WHY VIVEKANANDA INTERNATIONAL SCHOOL?</h1>
+                <p>Old School Values. New Age Methodologies</p>
+                <div className='home-hwd'>
+                    <div>
+                        <div>
+                            <img src={hwd1} alt='hwd1'/>
+                        </div>
+                        <p>Career Ready</p>
+                    </div>
+                    <div>
+                        <div>
+                            <img src={hwd2} alt='hwd2'/>
+                        </div>
+                        <p>Child & Safty</p>
+                    </div>
+                    <div>
+                        <div>
+                            <img src={hwd3} alt='hwd3'/>
+                        </div>
+                        <p>Leadership Skills</p>
+                    </div>
+                    <div>
+                        <div>
+                            <img src={hwd4} alt='hwd4'/>
+                        </div>
+                        <p>Successful Board Results</p>
+                    </div>
+                    <div>
+                        <div>
+                            <img src={hwd5} alt='hwd5'/>
+                        </div>
+                        <p>Well Trained Faculty</p>
+                    </div>
+                </div>
+            </div>
+
             <div className='home-sec-con'>
                 <div className='home-sec-con-img'>
                     <img src={weltvis} alt='weltovis'/>
@@ -48,7 +188,25 @@ function Home(){
                         </div>
                     </div>
                 </div>
+            </div>
 
+            <div className='homepage-msgch'>
+                <div>
+                    <h1>Message From Chairman</h1>
+                    <img src={cm1} alt='cm1'/>
+                    <h3>Dr. Koradala Naresh</h3>
+                    <p>Meet our visionary director, Dr.KORADALA NARESH, guiding our educational mission with expertise and
+                    passion.</p>
+                    <p style={{color:'#f57a25',textAlign:'right',fontWeight:500,cursor:'pointer'}} onClick={()=>navigate('/about')}>know more {`>`}</p>
+                </div>
+                <div>
+                    <h1>Message From correspondent</h1>
+                    <img src={cm2} alt='cm2'/>
+                    <h3>Smt. Lavanya Naresh</h3>
+                    <p>Discover the driving force behind our institution, Smt. Lavanya Naresh, committed to excellence in
+                    education.</p>
+                    <p style={{color:'#f57a25',textAlign:'right',fontWeight:500,cursor:'pointer'}} onClick={()=>navigate('/about')}>know more {`>`}</p>
+                </div>
             </div>
             <Footer/>
         </>
