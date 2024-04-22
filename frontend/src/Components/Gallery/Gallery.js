c3568d9162348f066afab84c7ffa7fa7742f33c6
PanchaliUpendra <upendrapanchali83@gmail.com>
2024-04-22 15:45:42 +0530

    gallary page completed successfully

--- frontend/src/Components/Gallery/Gallery.js
+++ frontend/src/Components/Gallery/Gallery.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import './Gallery.css';
 import Promo from './Galleryasset/promo.mp4';
 import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
@@ -17,6 +17,7 @@ import g9 from './Galleryasset/g9.jpg';
 import g10 from './Galleryasset/g10.jpg';
 
 function Gallery(){
+    const [cptovdo,setcptovdo] = useState(false);
     useEffect(()=>{
         window.scrollTo({top:0,behavior:'instant'});
     },[]);
@@ -37,8 +38,14 @@ function Gallery(){
                             designed to showcase and present artistic, thematic, or informational material 
                             in a coherent and aesthetically pleasing manner.</p>
                     </div>
+                    <div className='gallery-phots-videos-btn'>
+                        <button className={`${cptovdo===false?'activeb':''}`} onClick={()=>setcptovdo(false)}>Photos</button>
+                        <button className={`${cptovdo===false?'':'activeb'}`} onClick={()=>setcptovdo(true)}>videos</button>
+                    </div>
                     <div className='responsive-masonry-div'>
-                    <ResponsiveMasonry
+                        {
+                            cptovdo===false?
+                            <ResponsiveMasonry
                         columnsCountBreakPoints={{350: 1,450:2, 699: 3}} gutter="1rem"
                     >
                         <Masonry>
@@ -73,7 +80,45 @@ function Gallery(){
                             <img src={g10} alt="gallery-pics"/>
                         </div>
                         </Masonry>
-                    </ResponsiveMasonry>
+                    </ResponsiveMasonry>:
+                    <ResponsiveMasonry
+                    columnsCountBreakPoints={{350: 1,450:2, 699: 3}} gutter="1rem"
+                >
+                    <Masonry>
+                    {/* <div className='gallery-img-inner-item'>
+                        <img src={g1} alt="gallery-pics"/>
+                    </div>
+                    <div className='gallery-img-inner-item'>
+                        <img src={g2} alt="gallery-pics"/>
+                    </div>
+                    <div className='gallery-img-inner-item'>
+                        <img src={g3} alt="gallery-pics"/>
+                    </div> */}
+                    <div className='gallery-img-inner-item'>
+                    <iframe  src="https://www.youtube.com/embed/l62SIcbCPwo?si=ZqQApQtQ1EQcRBpo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                    </div>
+                    {/* <div className='gallery-img-inner-item'>
+                        <img src={g5} alt="gallery-pics"/>
+                    </div>
+                    <div className='gallery-img-inner-item'>
+                        <img src={g6} alt="gallery-pics"/>
+                    </div>
+                    <div className='gallery-img-inner-item'>
+                        <img src={g7} alt="gallery-pics"/>
+                    </div>
+                    <div className='gallery-img-inner-item'>
+                        <img src={g8} alt="gallery-pics"/>
+                    </div>
+                    <div className='gallery-img-inner-item'>
+                        <img src={g9} alt="gallery-pics"/>
+                    </div>
+                    <div className='gallery-img-inner-item'>
+                        <img src={g10} alt="gallery-pics"/>
+                    </div> */}
+                    </Masonry>
+                </ResponsiveMasonry>
+                        }
+                    
                     </div>
                     
 
